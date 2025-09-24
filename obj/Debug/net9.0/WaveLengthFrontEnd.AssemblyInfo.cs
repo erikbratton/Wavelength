53b0bfcbee0397555863df6d5e8868ae5c312239
Erik Bratton <erikbratton@gmail.com>
2025-09-24 15:29:35 -0400

    Wavelength front end updates

--- obj/Debug/net9.0/WaveLengthFrontEnd.AssemblyInfo.cs
+++ obj/Debug/net9.0/WaveLengthFrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaveLengthFrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65225a524d9b2e3302a03391225c4e03aee4aef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaveLengthFrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaveLengthFrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
